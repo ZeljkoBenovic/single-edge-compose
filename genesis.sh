48ea1970c8b6c6eb520a068e67e95ed64f61330b
ZeljkoBenovic <47507986+ZeljkoBenovic@users.noreply.github.com>
2023-11-14 20:27:04 +0100

    init commit

---
+++ genesis.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -f "/data/genesis.json" ]; then
+  exit 0
+fi
+
+apk add jq
+
+EDGE="/usr/local/bin/polygon-edge"
+GENESIS="genesis"
+NODE_ID="$($EDGE secrets output --data-dir /data --json | jq -r '.node_id')"
+NODE_ADDRESS="$($EDGE secrets output --data-dir /data --json | jq -r '.address')"
+
+DEFAULTS="--block-gas-limit 30000000 --epoch-size 100000 --chain-id 51001 --name polygon-edge"
+PARAMS="--dir /data/genesis.json --ibft-validator-type ecdsa --ibft-validator $NODE_ADDRESS"
+BOOTNODE="--bootnode /dns4/polygon-edge/tcp/1478/p2p/$NODE_ID"
+
+$EDGE $GENESIS $DEFAULTS $PARAMS $BOOTNODE
